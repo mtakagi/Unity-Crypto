7e958ea70fc3c163b0d733dad386a113d89113f2
mtakagi <mtakagi127@live.jp>
2020-01-21 16:35:32 +0900

    RC4 に対する攻撃のテストを実装

---
+++ Assets/Scripts/ARC4Attack/ARC4Attack.cs
@@ -0,0 +1,34 @@
+﻿// http://inaz2.hatenablog.com/entry/2013/11/30/233649
+
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+public static class ARC4Attack
+{
+    public static Dictionary<byte, int> Attack(byte[] message)
+    {
+        var dict = new Dictionary<byte, int>(256);
+        var key = new byte[128];
+
+        using (var rng = new RNGCryptoServiceProvider())
+        {
+            for (var i = 0; i < 65536; i++)
+            {
+                rng.GetBytes(key);
+                var enc = Crypto.ARC4.Encode(message, key);
+                var k = enc[1];
+                if (dict.ContainsKey(k))
+                {
+                    dict[k] += 1;
+                }
+                else
+                {
+                    dict[k] = 1;
+                }
+            }
+
+        }
+
+        return dict;
+    }
+}
---
+++ Assets/Scripts/ARC4Attack/ARC4AttackTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class ARC4AttackTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var dict = ARC4Attack.Attack(System.Text.Encoding.UTF8.GetBytes("this_is_a_message"));
+        var result = dict.OrderByDescending(e => e.Value).Take(5);
+
+        foreach (var kvp in result)
+        {
+            Debug.LogFormat($"{System.Text.Encoding.UTF8.GetString(new byte[] { kvp.Key })}: {kvp.Value}");
+        }
+    }
+}
